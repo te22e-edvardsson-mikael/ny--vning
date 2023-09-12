6f63dbd3811554ca93dc236d19a3d4d9d589b8bf
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2023-09-12 10:23:44 +0200

    jyigvyhuhyuyiu

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿
+Console.Write("du ser ett svärld tar du den? ");
+string name = Console.ReadLine().ToLower();
+
+
+if (name == "ja")
+{
+    Console.WriteLine("Dina händer börjar skaka, vill du fortsätta");
+    string fortatt = Console.ReadLine();
+    if (fortatt == "ja")
+    {
+
+    Console.WriteLine("ditt svärld börjar skaka, det verkar som om ända sättet att stoppa de är genom att skriva ja eller ska du 'GE UPP?'");
+  
+ 
+    }
+     string 123 = Console.ReadLine();
+     if 123 == ("ja")
+  {
+    Console.WriteLine("joebiden123");
+  }
+
+
+   else if (name == "ge upp")
+{
+    Console.WriteLine("missnöld");
+}
+
+else{
+    Console.WriteLine("åsna");
+}
+
+  
+
+
+
+    
+}
+else if (name == "nej")
+{
+    Console.WriteLine("Då börjar fåglar jagga dig och du dör, Tryck ENTER för att börja om");
+
+}
+
+
+
+
+
+
+else 
+{
+    Console.WriteLine("joebiden123");
+}
+
+
+Console.WriteLine("Tryck ENTER för att avsluta.");
+Console.ReadLine();
